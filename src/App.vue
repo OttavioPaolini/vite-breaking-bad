d5d46fae1e93915780ef145cb8dc2090d5b05a4b
Ottavio8 <paolini.ottavio8@gmail.com>
2022-11-10 18:58:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+export default{
+
+}
+</script>
+
+<template>
+
+  <h1>Ciao funziono</h1>
+
+  <div class="container">
+  
+  </div>
+
+</template>
+
+<style lang="scss">
+@use "./components/style/general.scss" as *;
+@use "./components/style/partials/variables" as*;
+h1{
+  color: $second-font-color;
+}
+
+</style>
